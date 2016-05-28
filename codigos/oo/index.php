4e6745e1438f211a5c8b3c25ac53a491d8376aa2
Sandro Javiel <scjaviel@gmail.com>
2016-05-28 01:14:59 +0000

    Ultimos arquivos com a tabela trazendo info sobre as series

--- codigos/oo/index.php
+++ codigos/oo/index.php
@@ -9,8 +9,9 @@
 
 <?php
 require_once('seriado.php');
+require_once('head.php');
+
 $seriado = new Seriado('Shadow Hunter', 'Caçador das sombras');
-//print_r($seriado);
 $seriado->addTemporada(1,'Caçador de ossos');
 $seriado->addEpisodio(1, 1, 'Piloto', 48.4, 'bla bla bla 1');
 $seriado->addEpisodio(1, 2, 'Um novo episódio', 49.1, 'bla bla bla 2');
@@ -20,10 +21,22 @@ $seriado->addEpisodio(2, 1, 'Assassino 1', 48.4, 'bla bla bla 1');
 $seriado->addEpisodio(2, 2, 'Assassino 2', 49.1, 'bla bla bla 2');
 $seriado->addEpisodio(2, 2, 'Assassino 3 ', 49.1, 'bla bla bla 2');
 
-echo "<pre>";
-print_r($seriado->getTemporadas());
-echo "<br>Episodios da 1ª temporada<br>";
-print_r($seriado->getEpisodios(1));
-echo "</pre>";
+$mrrobot = new Seriado('Mr Robot', 'Hacker que trabalha numa empresa e investiga todos a sua volta');
+$mrrobot->addTemporada(1,'Anything');
+$mrrobot->addEpisodio(1, 1, 'Piloto', 48.4, 'bla bla bla 1');
+$mrrobot->addEpisodio(1, 2, 'Um novo episódio', 49.1, 'bla bla bla 2');
+
+$listaSeriados = array();
+array_push($listaSeriados, $seriado);
+array_push($listaSeriados, $mrrobot);
+?>
+
+<table width='400' border='1'>
+<tr><td>Série</td><td>Número de temporadas</td></tr>
+<?php
+for($i=0; $i< count($listaSeriados); $i++ ){
+    echo "<tr><td>".$listaSeriados[$i]->getNomeSeriado() ."</td><td>". $listaSeriados[$i]->getNumeroTemporadas()."</td></tr>";
+}
+
 ?>
 </body>
--- codigos/oo/seriado.php
+++ codigos/oo/seriado.php
@@ -5,15 +5,23 @@ class Seriado {
     private $temporadas = array();
     private $nome;
     private $id;
-    private $sinopse;    
+    private $sinopse;  
+    
         
     function __construct($nome, $sinopse){
         $this->nome = $nome;
         $this->sinopse = $sinopse;
         $this->id = sha1(time());
+        
     }
     public function addTemporada($id_temporada, $sinopse){
         array_push($this->temporadas, new Temporada($this->id, $id_temporada, $sinopse));
+    }
+    public function getNomeSeriado(){
+        return $this->nome;
+    }
+    public function getNumeroTemporadas(){
+        return count($this->temporadas);
     }
     function addEpisodio($numero_temporada, $ordem, $titulo, $duracao, $sinopse){
         $this->temporadas[$numero_temporada-1]->addEpisodio($ordem, $titulo, $duracao, $sinopse);
@@ -25,7 +33,7 @@ class Seriado {
         return $this->temporadas[$id_temporada-1]->getEpisodios();
     }
 
-    
+
 }
 
 ?>
